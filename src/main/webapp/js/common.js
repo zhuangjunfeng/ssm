3ae0793fc9835392dbcd207bcf93b9a436e49f1c
zhuangjunfeng <z13609504061@outlook.com>
2016-10-15 00:12:57 +0800

    模板更换，问题解决

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -11,8 +11,11 @@ function findTypeList(){
         success:function(data){
             var typeList=data.data;
             var typeHtml="";
+            var location="";
             $.each(typeList,function(i,n){
                 typeHtml= "<li class='current'><a href='javascript:;'>"+ n.dictName+"</a></li>";
+                location+="<li class='bc_icon'>当前位置</li><li><a href='javacript:;'>"
+                    + n.dictName+"</a></li>"
             });
             $.ajax({
                 type:"GET",
@@ -21,16 +24,24 @@ function findTypeList(){
                 data:{type:programs},
                 success: function (data) {
                     var typeList=data.data;
-                    $.each(typeList,function(i,n){
-                        if(i==0)
-                        {
-                            findNewsByType(n.dictName);
+                    $.each(typeList,function(i,n) {
+                        if (i == 0) {
+                            findNewsByType(n.dictName, location);
+                            typeHtml += "<li class='current_detail'><a  class='news-type' data-type='"
+                                + n.dictName + "'>"
+                                + n.dictName + "</a></li>";
                         }
-                        typeHtml+="<li class='current_detail'><a  class='news-type' data-type='"+ n.dictName+"'>"+n.dictName+"</a></li>";
+                        else {
+                        typeHtml += "<li><a  class='news-type' data-type='"
+                            + n.dictName + "'>"
+                            + n.dictName + "</a></li>";
+                    }
                     });
                     $("#show-program-list").html(typeHtml);
                     $(".news-type").click(function(){
-                        findNewsByType($(this).attr("data-type"))
+                        $(this).parent().siblings().removeClass("current_detail");
+                        $(this).parent().addClass("current_detail");
+                        findNewsByType($(this).attr("data-type"), location);
                     })
                 }
             });
@@ -52,15 +63,16 @@ function findNewsById(newsId){
                 +news.newsAuthor+"</span><span>发布时间："
                 +news.editorTime+"</span><div class='art_txt'>"
                 +news.newsContent+"</div></article>";
-           var location="<li><a href='javacript:;'>"
+           var location="<li class='bc_icon'>当前位置</li><li><a href='javacript:;'>"
                 +news.newsProgram+"</a></li><li><a href='javascript:;'>"
-                +news.newsType+"</a></li>"
+                +news.newsType+"</a></li><li>"
+                +news.newsTitle+ "</li>"
             $("#show-news").html(newsListHtml);
             $("#show-location").html(location);
         }
     });
 }
-function findNewsByType(newsType){
+function findNewsByType(newsType, Location){
     $.ajax({
         type:"POST",
         dataType:"json",
@@ -70,13 +82,24 @@ function findNewsByType(newsType){
         success: function (data) {
             var newsList=data.data;
             var newsListHtml="";
+            var location="";
+            if(newsList!=""){
             $.each(newsList,function(i,n){
                 newsListHtml=newsListHtml+"<li><a style='cursor:pointer' class='news-detail' data-id='"
                                + n.newsId+"'>"
                                + n.newsTitle+"</a><span>"
                                + n.editorTime+"</span></li>";
+                 location="<li class='bc_icon'>当前位置</li><li><a href='javacript:;'>"
+                    + n.newsProgram+"</a> </li><li> <a href='javascript:;'>"
+                    +newsType+"</a> </li>";
             });
+            }
+            else{
+                location+=Location+"<li> <a href='javascript:;'>"
+                    +newsType+"</a> </li>";
+            }
             $("#show-news").html(newsListHtml);
+            $("#show-location").html(location);
             $(".news-detail").click(function(){
                 findNewsById($(this).attr("data-id"));
             })
