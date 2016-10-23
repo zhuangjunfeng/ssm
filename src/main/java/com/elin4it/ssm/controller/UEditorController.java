d9e14a0d5a6268a225e35a015d4008a4fd902fc9
zhuangjunfeng <z13609504061@outlook.com>
2016-10-23 20:40:19 +0800

    版本更新

--- src/main/java/com/elin4it/ssm/controller/UEditorController.java
+++ src/main/java/com/elin4it/ssm/controller/UEditorController.java
@@ -15,8 +15,7 @@ public class UEditorController{
     @RequestMapping(value="/config")
     public void config(HttpServletRequest request, HttpServletResponse response) {
         response.setContentType("application/json");
-        String rootPath = request.getSession()
-                .getServletContext().getRealPath("/");
+        String rootPath = request.getSession().getServletContext().getRealPath("/");
         try {
             String exec = new ActionEnter(request, rootPath).exec();
             PrintWriter writer = response.getWriter();
