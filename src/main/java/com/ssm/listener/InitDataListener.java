407de6fcf0ad6fae61170363d285865d3b76b44e
鱼西 华 <semstouch@gmail.com>
2017-01-25 08:09:14 +0800

    新增log4j日志模块

--- src/main/java/com/ssm/listener/InitDataListener.java
+++ src/main/java/com/ssm/listener/InitDataListener.java
@@ -3,6 +3,7 @@ package com.ssm.listener;
 import com.ssm.pojo.Dictionary;
 import com.ssm.service.DictionaryService;
 import com.ssm.service.DictionaryService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.context.ServletContextAware;
@@ -18,13 +19,14 @@ import java.util.List;
  */
 @Controller
 public class InitDataListener  implements InitializingBean,ServletContextAware{
+    private static Logger logger = Logger.getLogger(InitDataListener.class);
+
     @Resource
     private DictionaryService dictionaryService;
     public void  afterPropertiesSet() throws Exception{}
     public void setServletContext(ServletContext servletContext){
-        System.out.println("-------------------系统初始化字典数据開始--------------");
         List<Dictionary> list=dictionaryService.findDictionary();
         servletContext.setAttribute("dicList",list);
-        System.out.println("-------------------系统初始化字典数据結束--------------");
+        logger.info("-------------初始化数据成功----------");
     }
 }
